bca7fdb7d82c40bb38e6135d6b7965f3d9bea134
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-08 07:37:12 +0200

    new changes

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,12 +1,18 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import {Provider} from "react-redux"
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
 export default function App() {
   return (
+    <Provider store={store}>
+
+
     <View style={styles.container}>
-      <Text>You are a mobile developer now!</Text>
       {/* <StatusBar style="auto" /> */}
+      <HomeScreen/>
     </View>
+    </Provider>
   );
 }
 
