fde20066c7fa8954e58a6f19efe4e128a1994353
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-08 07:37:57 +0200

    new changes

---
+++ uber-clone/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View,SafeAreaView,Image } from 'react-native'
+import React from 'react'
+import tw from "tailwind-react-native-classnames"
+import Logo from "./UberLogo.png"
+import NavOptions from '../components/NavOptions'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw` h-full`}>
+    <View style={tw`p-5`}>
+      <Image source={{uri:"https://Links.papareact.com/gzs"}}  style={{width:100,height:100,resizeMode:"contain"}}/>
+      <NavOptions />
+    </View>
+
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    text:{
+        color:"blue",
+    }
+})
