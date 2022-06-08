9e59039465520634bd4d1729cc6706ef43b8b5de
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-08 14:48:27 +0200

    new changes

--- uber-clone/components/NavOptions.js
+++ uber-clone/components/NavOptions.js
@@ -9,6 +9,7 @@ import {
 import React from "react";
 import tw from "tailwind-react-native-classnames"
 import {Icon} from "react-native-elements"
+import { useNavigation } from "@react-navigation/native";
 const data = [
   {
     id: "123",
@@ -24,20 +25,21 @@ const data = [
   },
 ];
 const NavOptions = () => {
+  const navigation=useNavigation()
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pt-4 bg-gray-200 m-2 pb-8 w-40`}>
+        <TouchableOpacity onPress={()=>navigation.navigate(item.screen)} style={tw`p-2 pl-6 pt-4 bg-gray-200 m-2 pb-8 w-40`}>
           <View>
             <Image
               source={{ uri: item.image }}
               style={{ width: 120, height: 120, resizeMode: "contain" }}
             />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-            <Icon type="antdesign" color="white" name="arrowright" style={tw`p-2 bg-black rounded-full w-10 mt-3.`}/>
+            <Icon type="antdesign" color="white" name="arrowright" style={tw`p-2 bg-black rounded-full w-10 mt-3`}/>
           </View>
         </TouchableOpacity>
       )}
