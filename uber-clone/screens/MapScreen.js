20e19aff224a0579070d22c7f90f133407e5c84b
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-08 14:49:51 +0200

    new changes

---
+++ uber-clone/screens/MapScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import MapView from "react-native-maps"
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>MapScreen</Text>
+    </View>
+  )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
