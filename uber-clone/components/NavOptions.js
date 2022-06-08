7399d5ffbae60031c5f268ef4c0cbdf8bd2b6a35
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-08 07:37:41 +0200

    new changes

---
+++ uber-clone/components/NavOptions.js
@@ -0,0 +1,48 @@
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames"
+import {Icon} from "react-native-elements"
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://Links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://Links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
+const NavOptions = () => {
+  return (
+    <FlatList
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pt-4 bg-gray-200 m-2 pb-8 w-40`}>
+          <View>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon type="antdesign" color="white" name="arrowright" style={tw`p-2 bg-black rounded-full w-10 mt-3.`}/>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
