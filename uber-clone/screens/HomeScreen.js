8c830410421a07a6f4fe3dbb6395a081ce3eca57
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-08 14:49:40 +0200

    new chan ges

--- uber-clone/screens/HomeScreen.js
+++ uber-clone/screens/HomeScreen.js
@@ -1,14 +1,36 @@
 import { StyleSheet, Text, View,SafeAreaView,Image } from 'react-native'
 import React from 'react'
 import tw from "tailwind-react-native-classnames"
-import Logo from "./UberLogo.png"
+import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete"
 import NavOptions from '../components/NavOptions'
-
+import {GOOGLE_MAP_APIKEY} from "@env";
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw` h-full`}>
     <View style={tw`p-5`}>
       <Image source={{uri:"https://Links.papareact.com/gzs"}}  style={{width:100,height:100,resizeMode:"contain"}}/>
+      <GooglePlacesAutocomplete 
+      placeholder="Where from?" 
+      styles={{
+        container:{
+          flex:0,
+        },
+        textInput:{
+          fontSize:18,
+        }
+      }} 
+      query={{
+        key:GOOGLE_MAP_APIKEY,
+        language:"en"
+      }}
+      onPress={(data,details=null)=>{console.log(data)}}
+      fetchDetails={true}
+      enablePoweredByContainer={false}
+      minLength={2}
+      nearbyPlacesAPI='GooglePlacesSearch' 
+    debounce={400} 
+
+      />
       <NavOptions />
     </View>
 
