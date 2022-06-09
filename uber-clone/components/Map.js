4a93db7b04bbe2f5877df3889c597ecfa7220e50
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-09 15:31:36 +0200

    new change

--- uber-clone/components/Map.js
+++ uber-clone/components/Map.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import tw from "tailwind-react-native-classnames"
 import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions'
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import {useSelector} from "react-redux"
-import {selectOrigin} from "../slices/navSlice"
+import {selectOrigin,selectDestination} from "../slices/navSlice"
+import {useRef} from "react"
+import {useEffect} from "react"
 export default function App() {
-    const origin =useSelector(selectOrigin)
+    const origin =useSelector(selectOrigin);
+    const destination=useSelector(selectDestination);
+    const mapRef=useRef(null);
+    useEffect(()=>{
+        if(!origin || !destination) return;
+        mapRef.current.fitToSuppliedMarkers(["origin","destnation"],{
+          edgePadding:{top:50,right:50,bottom:5,left:50},
+        })
+    },[origin,destination])
   return (
       <MapView style={tw`flex-1`}
       mapType="mutedStandard"
+      ref={mapRef}
       //  initialRegion={
       //     {
       //         latitude:37.78825,
@@ -23,6 +35,25 @@ export default function App() {
             coordinate={{
                 latitude:37.78825,
               longitude:-122.4324,
+            }}
+            title="Origin"
+            description="The user selecetd origin"
+            identifier='origin'
+        />
+        )}
+        {/* {origin && destination && <MapViewDirections
+          origin={origin}
+          destination={destination}
+          strokeWidth={3}
+          strokeColor={"black"}
+          apikey={"AIzaSyDMARlawEcd00HDP7EEfsWnHr5Yzfw86WM"}
+          
+        />} */}
+        {destination?.location && (
+            <Marker 
+            coordinate={{
+                latitude:57.78825,
+              longitude:-422.4324,
             }}
             title="Origin"
             description="The user selecetd origin"
