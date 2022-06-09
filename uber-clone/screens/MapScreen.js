4f223d2c0c4ee8bdafd4cff8a807a4dc3e80f71d
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-09 22:07:53 +0200

    new changes

--- uber-clone/screens/MapScreen.js
+++ uber-clone/screens/MapScreen.js
@@ -1,19 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import tw from "tailwind-react-native-classnames"
 import MapView from "react-native-maps"
 
+import { useNavigation } from "@react-navigation/native";
 import Map from '../components/Map'
 import NavigationCard from '../components/NavigationCard';
 import RideOptionsCard from '../components/RideOptionsCard';
 import {selectOrigin,selectDestination} from "../slices/navSlice"
 import {useSelector} from "react-redux"
+import {Icon } from "react-native-elements"
 const MapScreen = () => {
   const Stack=createNativeStackNavigator();
+  const navigation=useNavigation()
   const origin=useSelector(selectOrigin);
   return (
     <View>
+      <TouchableOpacity onPress={()=>navigation.navigate("HomeScreen")} style={tw`bg-gray-200 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}>
+        <Icon name="menu"/>
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
