edeeaa0b7379ca2721bdc5c1772a8820eff93643
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-09 15:31:42 +0200

    new change

--- uber-clone/components/NavigationCard.js
+++ uber-clone/components/NavigationCard.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View,SafeAreaView,TextInput,Button } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView,TextInput,Button,TouchableOpacity,Pressable } from 'react-native'
 import React from 'react'
 import {GOOGLE_MAPS_APIKEY} from "@env";
 import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete"
 import { useDispatch } from "react-redux";
+import {Icon} from "react-native-elements"
 import {setDestination,setOrigin} from "../slices/navSlice"
-import tw from "tailwind-react-native-classnames"
+  import NavFavourites from "../components/NavFavourites"
+  import tw from "tailwind-react-native-classnames"
 import { useNavigation } from "@react-navigation/native";
 
 const NavigationCard = () => {
@@ -39,15 +41,25 @@ const NavigationCard = () => {
 
       /> */}
          <TextInput
-        style={{height: 40}}
+        style={{height: 30,  backgroundColor:"#DDDDDF",}}
         placeholder="Where to?"
         onChangeText={newText =>{
-          dispatch(setOrigin({location:newText,description:`My origin is ${newText} `}));
-        dispatch(setDestination(setDestination(null)))
+        dispatch(setDestination({location:newText,description:`My destination  is ${newText} `}))
         }}
       />
-        <Button title="submit" onPress={submitHandler} />
+        <Pressable   onPress={submitHandler} style={tw`bg-blue-600 h-6 text-center`}><Text style={tw`text-center mt-0.5 text-white`}>Submit</Text></Pressable>
+        <NavFavourites />
       </View>
+        <View style={tw`flex-row flex justify-evenly bg-white mt-auto py-2 border-t border-gray-100`}>
+        <TouchableOpacity style={tw`flex flex-row w-24 bg-black px-4 py-3 rounded-full justify-between`} onPress={()=>{navigation.navigate("RideOptionsCard")}}>
+          <Icon name="car" type="font-awesome" color="white" size={16}/>
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={tw`flex flex-row w-24 justify-between w-24  px-4 py-3`}>
+          <Icon name="fast-food-outline" type="ionicon" color="black" size={16}/>
+          <Text style={tw`text-black text-center`}>Eats</Text>
+        </TouchableOpacity>
+        </View>
     </SafeAreaView>
   )
 }
