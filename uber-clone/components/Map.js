866aa5d7ef9c40e47523e9a191c712e57a596c3c
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-09 22:07:48 +0200

    new changes

--- uber-clone/components/Map.js
+++ uber-clone/components/Map.js
@@ -3,12 +3,13 @@ import tw from "tailwind-react-native-classnames"
 import MapView, { Marker } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions'
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import {useSelector} from "react-redux"
-import {selectOrigin,selectDestination} from "../slices/navSlice"
+import {useSelector,useDispatch} from "react-redux"
+import {selectOrigin,selectDestination,setTravelTimeInformation} from "../slices/navSlice"
 import {useRef} from "react"
 import {useEffect} from "react"
 export default function App() {
     const origin =useSelector(selectOrigin);
+    const dispatch=useDispatch()
     const destination=useSelector(selectDestination);
     const mapRef=useRef(null);
     useEffect(()=>{
@@ -17,6 +18,10 @@ export default function App() {
           edgePadding:{top:50,right:50,bottom:5,left:50},
         })
     },[origin,destination])
+    useEffect(()=>{
+      if(!origin || !destination) return;
+      dispatch(setTravelTimeInformation({distance:{text:"470 mi",value:75640,},duration:{text:"8 hours 7 min",value:29240},statues:"Ok"}))
+    })
   return (
       <MapView style={tw`flex-1`}
       mapType="mutedStandard"
