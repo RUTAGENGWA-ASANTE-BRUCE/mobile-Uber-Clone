0260318b323658d278f80a276888b616e80db65b
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-22 11:33:28 +0200

    new change

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -7,31 +7,4 @@ import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
 import { store } from './store';
 import "react-native-gesture-handler"
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-export default function App() {
-  const Stack=createNativeStackNavigator();
-  return (
-    <Provider store={store}>
-
-    <NavigationContainer>
-    <SafeAreaProvider>
-
-    <KeyboardAvoidingView
-     style={{flex:1}}
-     behavior={Platform.OS==="ios"?"padding":"height"}
-     keyboardVerticalOffset={Platform.OS==="ios"?-64:0}>
-
-      <Stack.Navigator>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown:false}}/>
-        <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown:false}}/>
-      </Stack.Navigator>
-
-  
-    </KeyboardAvoidingView>
-    </SafeAreaProvider>
-    </NavigationContainer>
-    </Provider>
-  );
-}
+import { NavigationContainer }
